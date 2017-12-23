95141274efce261267a212a8fcdc1ed81d4fe0e4
beingcompiled <beingcompiled@gmail.com>
2017-12-23 14:39:33 -0500

    added router transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
 	<div id='app'>
 		<app-header v-bind:nav='nav'></app-header>
-		<router-view/>
+		<transition name='fade' mode='out-in' v-on:after-enter='afterEnter' appear>
+			<!--  
+				Vue Router does not register any routing change if the same component is being used. 
+				Using key, any change to the path will trigger a reload of the component with the new data.
+			-->
+			<router-view :key='$route.fullPath'/>
+		</transition>
 		<app-footer v-bind:links='links'></app-footer>
 	</div>
 </template>
@@ -24,7 +30,12 @@
 			}
 		},
 		created: function () {
-			console.log('App created:: ')
+			console.log('App created!')
+		},
+		methods: {
+			afterEnter: function (el) {
+				console.log('App route entered:', this.$route.name)
+			}
 		}
 	}
 </script>
